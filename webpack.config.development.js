92315876b3221c56434ad20871ffca96ff7702f2
RequireSun <kelvinsen@outlook.com>
2017-01-16 01:33:49 +0800

    atempting change to webpack

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -8,6 +8,10 @@ const baseConfig = require('./webpack.config.base');
 
 const config = Object.create(baseConfig);
 
+config.output = Object.assign(config.output, {
+    outputDir     : './dist',
+    outputFilename: 'html-to-jsx.js',
+});
 config.devtool = '#cheap-module-source-map';
 config.devServer = { hot: true, inline: true, };
 config.plugins.push(
